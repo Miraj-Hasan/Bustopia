f94fe22e2e99dcd92c9bf493179e4fc63e33e545
fihad <hellofihad@gmail.com>
2025-06-23 02:38:52 +0600

    ci/cd fix cd condition 3

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@ set -e
 
 cd ~/SWE/Bustopia
 
+# Stop and remove all running containers, networks, and orphaned stuff
+echo "[0/3] Stopping and removing existing containers, networks, and orphaned stuff"
+docker-compose down --volumes --remove-orphans
+
+# Optionally: prune dangling images
+docker image prune -f
+
 echo "[1/3] Pulling newest images from Docker Hub"
 docker-compose pull
 
